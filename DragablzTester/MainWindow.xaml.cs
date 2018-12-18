638654135541247bb16aee40c44b124cce1207a3
sawada_toyohito <sawada_toyohito@mdv.co.jp>
2018-12-18 18:24:02 +0900

    プロジェクト ファイルを追加します。

---
+++ DragablzTester/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Dragablz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragablzTester
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            var viewModel = new MainWindowViewModel
+                (
+                    new HeaderedItemViewModel { Header = "HOME", Content = "", IsSelected = true }      
+                );
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ MetroTester/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MetroRadiance.UI;
+using System.Windows;
+
+namespace MetroTester
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Window Close時にシステムを終了する。
+            base.OnStartup(e);
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+            // テーマサービスの登録を行う。
+            ThemeService.Current.Register(this, Theme.Windows, Accent.Windows);
+        }
+    }
+}
